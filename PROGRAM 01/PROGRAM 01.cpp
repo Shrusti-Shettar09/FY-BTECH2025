d945765b13f5de46cfbdbeba44e2b09c46dccd80
Shrusti-Shettar09 <shrustishettar0907@gmail.com>
2025-09-26 22:02:32 +0530

    Update PROGRAM 01.cpp

--- PROGRAM 01/PROGRAM 01.cpp
+++ PROGRAM 01/PROGRAM 01.cpp
@@ -1,1 +1,23 @@
+#include <iomanip>
+#include <iostream>
+using namespace std;
+int main()
+{
+    int x;
+    cout<<"enter a number"<<endl;
+    cin>>x;
+    cout<<"hexagonal value"<<hex<<x<<endl;
+    cout<<"hexagonal decimal"<<dec<<x<<endl;
+    cout<<"octagonal value"<<oct<<x<<endl;
+    cout<<"octagonal decimal"<<dec<<x<<endl;
+    return 0;
+}
 
+
+OUTPUT :
+enter a number
+999
+hexagonal value3e7
+hexagonal decimal999
+octagonal value1747
+octagonal decimal999
