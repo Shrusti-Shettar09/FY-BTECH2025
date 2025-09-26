20ec914d196df7ba6f5a8b6d8e6c38e0f1c9d76e
Shrusti-Shettar09 <shrustishettar0907@gmail.com>
2025-09-26 22:05:02 +0530

    Create PROGRAM 3.cpp

---
+++ PROGRAM 3/PROGRAM 3.cpp
@@ -0,0 +1,25 @@
+#include<iostream>
+#include<cmath>
+#define PI 3.14
+using namespace std;
+int main()
+{
+    cout<<"#include<iostream>"<<endl;
+    cout<<"#include<cmath>"<<endl;
+    const int MAX=1000;
+    cout<<"PI="<<PI;
+    cout<<"MAX="<<MAX;
+    cout<<"cout<<hello world"<<endl;
+    cout<<"cin>>x"<<endl;
+    cout<<"sqrt(16)"<<sqrt(16);
+    cout<<"pow(2,3)"<<pow(2,3);
+    return 0;
+}
+
+output:#include<iostream>
+#include<cmath>
+PI=3.14MAX=1000cout<<hello world
+cin>>x
+sqrt(16)4pow(2,3)8
+
+=== Code Execution Successful ===
